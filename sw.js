1d1d92c8a450dd886408e4372f45d1943c209660
Ramsol <52590842+wooramsol@users.noreply.github.com>
2025-06-28 01:46:42 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,16 @@
 const CACHE_NAME = 'modurooftop-cache-v1';
 const FILES = [
-  '/', '/index.html', '/manifest.json',
-  '/icon-192.png', '/icon-512.png'
+  '/', '/index.html'
 ];
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(FILES))
+  );
 });
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });

b974c9a579884729f0840f1109a6f76ceaf93709
Ramsol <52590842+wooramsol@users.noreply.github.com>
2025-06-28 01:48:48 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'modurooftop-cache-v1';
+const FILES = [
+  '/', '/index.html', '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
